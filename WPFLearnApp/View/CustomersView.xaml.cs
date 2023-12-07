aa14b115becfc74618d878682ceb04c73aae3b06
Yard0 <1997yaroslavnesenchuk1997@gmail.com>
2023-12-07 13:35:47 +0200

    Binded FirstName

--- WPFLearnApp/View/CustomersView.xaml.cs
+++ WPFLearnApp/View/CustomersView.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WPFLearnApp.Data;
+using WPFLearnApp.ViewModel;
 
 namespace WPFLearnApp.View
 {
@@ -20,10 +10,21 @@ namespace WPFLearnApp.View
     /// </summary>
     public partial class CustomersView : UserControl
     {
+        private CustomersViewModel _viewModel;
+
         public CustomersView()
         {
             InitializeComponent();
+            _viewModel = new CustomersViewModel(new CustomerDataProvider());
+            DataContext = _viewModel;
+            Loaded += CustomersView_Loaded;
         }
+
+        private async void CustomersView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
